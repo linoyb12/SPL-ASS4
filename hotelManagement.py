3a5f8cd669c42f8cd7c8446c7bf49f79d0888d3f
linoyb12 <linoybar@post.bgu.ac.il>
2017-01-23 11:12:45 +0200

    finished hotelManagment -have problem with _tasks

--- hotelManagement.py
+++ hotelManagement.py
@@ -1,11 +1,11 @@
 import sqlite3
 import sys
 
+_tasks = 0
 _conn = sqlite3.connect('cronhoteldb.db')
 
-def parseConfig(configPath):
 
-def createTables:
+def create_tables():
     _conn.executescript("""
         CREATE TABLE TaskTimes(
             TaskId INTEGER PRIMARY KEY NOT NULL,
@@ -36,32 +36,55 @@ def createTables:
         );
     """)
 
+
 def room(splited_data):
+    _conn.execute("""
+        INSERT INTO Rooms (RoomNumber) VALUES (?,)""", [splited_data[1], ])
 
-def breakfast(splited_data):
+    if splited_data.__len__() > 2:
+        _conn.execute("""
+            INSERT INTO Residents(RoomNumber, FirstName, LastName) VALUES (?, ?, ?)""", [splited_data[1],
+                                                                                         splited_data[2],
+                                                                                         splited_data[3]])
 
-def wakeup(splited_data):
+
+def breakfast_n_wakup(splited_data):
+    _conn.execute("""
+            INSERT INTO TaskTimes (TaskId, DoEvery, NumTimes) VALUES (?, ?, ?)""", [_tasks, splited_data[1], splited_data[3]])
+    _conn.execute("""
+        INSERT INTO Tasks (TaskId, TaskName, Parameter) VALUES (?, ?, ?)""", [_tasks, splited_data[0], splited_data[2]])
+    _tasks += 1
+
 
 def clean(splited_data):
+    _conn.execute("""
+            INSERT INTO TaskTimes (TaskId, DoEvery, NumTimes) VALUES (?, ?, ?)""", [_tasks, splited_data[1], splited_data[3] ])
+    _conn.execute("""
+        INSERT INTO Tasks (TaskId, TaskName, Parameter) VALUES (?, ?, ?)""", [_tasks, splited_data[0], 0 ])
+    _tasks += 1
 
-def insertData(data):
-    splited_data=str.split(',')
-    options={
-        'room':room(splited_data),
-        'clean':clean(splited_data),
-        'wakeup':wakeup(splited_data),
-        'breakfast':breakfast(splited_data),
+
+def insert_data(data):
+    splited_data=data.split(',')
+    options = {
+        'room': room(splited_data),
+        'clean': clean(splited_data),
+        'wakeup': breakfast_n_wakup(splited_data),
+        'breakfast': breakfast_n_wakup(splited_data),
     }
-    options[splited_data[0]]()
+    options[splited_data[0]](splited_data)
 
-def main(configPath):
-    parseConfig(configPath)
+
+def parse_config(config_path):
+    with open(config_path) as input_file:
+        for line in input_file:
+            insert_data(line)
+
+
+def main(config_path):
+    parse_config(config_path)
 
 
 if __name__ == '__main__':
     main(sys.argv)
-
-
-
-
 
